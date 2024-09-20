6562defd4f39a2f6105ed1c3af5b8cc502671f07
PyCMC <admin@pycmc.eu>
2024-09-20 18:58:22 +0000

    Some changes

--- src/main/java/pycmc/pnxcustom/TranslationProvider.java
+++ src/main/java/pycmc/pnxcustom/TranslationProvider.java
@@ -3,7 +3,9 @@ package pycmc.pnxcustom;
 import cn.nukkit.Player;
 
 public abstract class TranslationProvider {
+    @Deprecated
     public abstract String translate(String lang, String key);
+    @Deprecated
     public abstract String translate(String lang, String key, String... params);
 
     public abstract String translate(Player p, String key);
